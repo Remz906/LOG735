65c1acf7e016e1ab6ccfed67391d17830af269e3
remz <remz906@gmail.com>
2018-07-10 16:23:05 -0400

    Clean up imports.

--- log735-api/src/main/java/Main.java
+++ log735-api/src/main/java/Main.java
@@ -1,11 +1,8 @@
-import arreat.api.DataBase;
-import arreat.api.core.NetService;
+import arreat.impl.DataBase;
+import arreat.impl.core.NetService;
 
 import java.net.UnknownHostException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.Scanner;
 
 /**
@@ -43,7 +40,7 @@ public class Main {
             boolean loop = true;
 
             while (loop) {
-                String value = scanner.next();
+                String value = scanner.nextLine();
 
                 loop = !"exit".equalsIgnoreCase(value);
 
