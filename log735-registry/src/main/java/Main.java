44880399beb072732a2cc140e874a8f162fa2d57
Michael Buron Yuen <michael.buron.yuen@gmail.com>
2018-07-17 22:08:59 -0400

    WIP : server

---
+++ log735-registry/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.net.UnknownHostException;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws UnknownHostException {
+        if (args.length > 0) {
+
+        } else {
+
+        }
+    }
+}
